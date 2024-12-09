39afff7fc3921e96a27b1d6d776527be39b31982
William Woodard <williamwoodard98@gmail.com>
2024-12-09 15:53:46 -0600

    creating and challenge 1

---
+++ Program.cs
@@ -0,0 +1,43 @@
+/* 
+Challenge 1: Data Types
+    Declares variables of different types (int, double, string).
+    Initializes them and prints their values.
+ */
+
+class Program1
+{
+    static void Main(string[] args)
+    {
+        int intNumber = 1;
+        double doubleNumber = 2.2;
+        string stringExample = "Example";
+
+        Console.WriteLine($"Integer: {intNumber}");
+        Console.WriteLine($"Double: {doubleNumber}");
+        Console.WriteLine($"String: {stringExample}");
+    }
+}
+
+/* 
+Challenge 2: Collections
+    Create a List<int> and add numbers 1 to 10.
+    Use a foreach loop to display the values.
+ */
+
+
+
+
+ /* 
+ Challenge 3: Loops
+    Write a for loop that prints all even numbers from 0 to 20.
+    Write a while loop that counts down from 5 to 0.
+  */
+
+
+  /* 
+  Challenge 4: Conditional Statements
+    Write a program that asks for the user's age and prints:
+    "Child" if age < 12.
+    "Teenager" if age >= 12 and < 18.
+    "Adult" if age >= 18.
+   */
